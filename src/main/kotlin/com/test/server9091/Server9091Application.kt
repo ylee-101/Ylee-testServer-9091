d0b70f07dc01ecbb364c2358f1a92d2d7035b16c
ylee <ylee.develop@gmail.com>
2026-09-16 13:17:14 +0900

    init project

---
+++ src/main/kotlin/com/test/server9091/Server9091Application.kt
@@ -0,0 +1,11 @@
+package com.test.server9091
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Server9091Application
+
+fun main(args: Array<String>) {
+	runApplication<Server9091Application>(*args)
+}
---
+++ src/test/kotlin/com/test/server9091/Server9091ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.test.server9091
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Server9091ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
